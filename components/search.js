accf67cced1c16494b29a97bca79d20971680905
antonydeepnz <mashoshini@gmail.com>
2020-03-06 11:36:14 +0300

    1 file modified

--- components/search.js
+++ components/search.js
@@ -11,8 +11,9 @@ const Wrapper = styled.div`
 `
 
 const Input = styled.input`
-  display: ${props => props.show? 'block' : 'none'};
+  visibility: ${props => props.show? 'visible' : 'hidden'};
   width: ${props => props.show? '150px' : '0px'};
+  transition: width 0.5s, visibility 0.5s;
 `
 
 const seacrhIcon = <><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></>
@@ -21,11 +22,10 @@ const Search = ({}) => {
   const [ show, setShow ] = useState(false)
 
   const handleIconClick = () => {
-    console.log('sdfsdf')
-    setShow(true)
+    setShow(!show)
   }
   return(
-    <Wrapper onMouseEnter={handleIconClick}>
+    <Wrapper onMouseEnter={handleIconClick} onMouseLeave={handleIconClick}>
       <SVGIcon width={24} height={24}>
         { seacrhIcon }
       </SVGIcon>
