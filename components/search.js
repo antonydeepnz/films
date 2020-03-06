2dec0cf96187a47166f007c107b6a8d248c84f53
antonydeepnz <mashoshini@gmail.com>
2020-03-06 11:44:47 +0300

    1 file modified, 1 added

--- components/search.js
+++ components/search.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import SVGIcon from './icon'
+import { seacrhIcon } from '../utils/icons'
 
 const Wrapper = styled.div`
   position: relative;
   display: flex;
+  align-items: center;
+  background-color: lightblue;
   margin: 3px;
   border-radius: 15px;
+  height: 35px;
+  width: ${props => props.show? '250px' : '24px'};
+  transition: width 0.5s;
 `
 
 const Input = styled.input`
@@ -16,8 +22,6 @@ const Input = styled.input`
   transition: width 0.5s, visibility 0.5s;
 `
 
-const seacrhIcon = <><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></>
-
 const Search = ({}) => {
   const [ show, setShow ] = useState(false)
 
@@ -25,7 +29,9 @@ const Search = ({}) => {
     setShow(!show)
   }
   return(
-    <Wrapper onMouseEnter={handleIconClick} onMouseLeave={handleIconClick}>
+    <Wrapper show={show} 
+      onMouseEnter={handleIconClick} 
+      onMouseLeave={handleIconClick} >
       <SVGIcon width={24} height={24}>
         { seacrhIcon }
       </SVGIcon>
