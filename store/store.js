a4a92429d1cfb0059021f772c423e1144f01832c
antonydeepnz <mashoshini@gmail.com>
2020-03-10 10:05:15 +0300

    1 file modified

--- store/store.js
+++ store/store.js
@@ -2,7 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
 import films from '../reducers.films'
+import { getGenres } from './sagas/sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 const reducer = combineReducers({films})
 
-export default createStore (reducer, applyMiddleware(createSagaMiddleware))
+export default createStore (reducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(getGenres)
